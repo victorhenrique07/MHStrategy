04f96f3b8b701c0f375f16ad17dbcec238e84586
Victor Henrique Silva <henrictor@hotmail.com>
2022-12-23 00:19:59 -0300

    monsters/ and monsters/id ready

--- app/mhwapi/urls.py
+++ app/mhwapi/urls.py
@@ -4,4 +4,6 @@ from mhwapi import views
 urlpatterns = [
     path('/', views.home, name="home"),
     path('monsters/', views.monsters, name="monsters"),
+    path('monsters/<int:id>', views.get_monster, name="get monster"),
+    path('monsters/create/<int:id>', views.register_monster, name="register monster")
 ]
--- app/mhwapi/views.py
+++ app/mhwapi/views.py
@@ -1,28 +1,48 @@
+from .models import Monsters
 from .forms import MonsterForm
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import json
 
 # Create your views here.
 def home(request):
     return HttpResponse("Teste")
 
 def monsters(request):
-    api_url = "https://mhw-db.com/monsters"
-    response = requests.get(api_url)
-    response.json()
-    data = []
-    for i in response:
-        data.append(i["name"])
-    return HttpResponse(response)
+    try:
+        api_url = "https://mhw-db.com/monsters"
+        response = requests.get(api_url)
+        data = response.json()
+        monster_list = []
+        for i in data:
+            monster_list.append(i["name"])
+        json_teste = json.dumps(monster_list)
+        return HttpResponse(json_teste)
+    except Exception as e:
+        print(e)
+        return HttpResponse(e)
 
-def monsters(request):
-    import json
-    with open('mhwapi/monsters.json', encoding='utf-8') as monsters_json:
-        data = json.load(monsters_json)
-        
-    item = []
-    for i in data:
-        item.append(i["name"])
-    data = json.dumps(item)
-    return HttpResponse(item)
+
+def get_monster(request, id):
+    api_url = f"https://mhw-db.com/monsters/{id}"
+    response = requests.get(api_url)
+    data = response.json()
+    return HttpResponse(data["id"], data["name"])
+
+
+def register_monster(request, id):
+    try:
+        form = MonsterForm(request.POST)
+        if request.method == "POST":
+            if form.is_valid():
+                api_url = f"https://mhw-db.com/monsters/{id}"
+                response = requests.get(api_url)
+                data = response.json()
+                json_data = json.dumps(data)
+                form.save()
+                form = MonsterForm()
+                return HttpResponse(json_data, content_type='application/json')
+    except Exception as e:
+        print(e)
+        return HttpResponse(e)
